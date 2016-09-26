66ade3b532c87a6a8139dff7b4d74adb1ef4ebac
Zina K <zina.kuzmin@gmail.com>
2016-09-26 19:13:05 +0300

    add file

---
+++ App/app/src/main/java/zina_eliran/app/API/Listeners/OnSetValueCompleteListener.java
@@ -0,0 +1,31 @@
+package zina_eliran.app.API.Listeners;
+
+import android.util.Log;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import zina_eliran.app.API.DAL;
+import zina_eliran.app.BusinessEntities.BEResponse;
+import zina_eliran.app.BusinessEntities.BEResponseStatusEnum;
+
+/**
+ * Created by Zina K on 9/23/2016.
+ */
+
+public class OnSetValueCompleteListener implements Firebase.CompletionListener{
+
+    @Override
+    public void onComplete(FirebaseError firebaseError, Firebase firebase) {
+        BEResponse response = new BEResponse();
+
+        if (firebaseError != null) {
+            response.setStatus(BEResponseStatusEnum.error);
+            response.setMessage(firebaseError.getMessage());
+        } else {
+            response.setStatus(BEResponseStatusEnum.success);
+            Log.e("OnComplete", firebase.toString());
+        }
+        DAL.setActionResponse(response);
+    }
+}

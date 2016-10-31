61a8c04979a1a1b7fff75cb0c40009cdad5cfee9
Zina K <zina.kuzmin@gmail.com>
2016-10-31 23:07:25 +0200

    email sender on user registration works. Added external jars that are used in email sender

---
+++ App/app/src/main/java/zina_eliran/app/API/EmailSender/EmailSendThread.java
@@ -0,0 +1,29 @@
+package zina_eliran.app.API.EmailSender;
+
+import zina_eliran.app.BusinessEntities.CMNLogHelper;
+
+/**
+ * Created by Zina K on 10/30/2016.
+ */
+
+public class EmailSendThread implements Runnable{
+    private String emailAdress;
+    private String name;
+    private String verificationCode;
+
+    public EmailSendThread(String emailAdress, String name, String verificationCode){
+        this.emailAdress = emailAdress;
+        this.name = name;
+        this.verificationCode = verificationCode;
+    }
+
+    @Override
+    public void run() {
+        try {
+            EmailSender send = new EmailSender(emailAdress, name , verificationCode);
+            CMNLogHelper.logError("SendMailSucceed", "mail sent");
+        } catch (Exception e) {
+            CMNLogHelper.logError("SendMailFailed", e.getMessage());
+        }
+    }
+}
---
+++ App/app/src/main/java/zina_eliran/app/Notifications/GCMRegistrationIntentService.java
@@ -0,0 +1,15 @@
+package zina_eliran.app.Notifications;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Zina K on 10/29/2016.
+ */
+//
+//public class GCMRegistrationIntentService extends IntentService{
+//    @Override
+//    protected void onHandleIntent(Intent intent) {
+//
+//    }
+//}
